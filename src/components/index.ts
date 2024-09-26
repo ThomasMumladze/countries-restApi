f50268cd3491abaa30aa95bec73fcb320534b52e
Thomas Mumladze <a.mumladze@sda.gov.ge>
2024-09-26 17:13:45 +0400

    import

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,26 @@
 import Button from "./Ui/Button";
 import H1 from "./Ui/H1";
+import Input from "./Ui/Input";
 
-const Ui = {
+export const Ui = {
     Button,
     H1,
+    Input,
 };
 
 export default Ui;
+
+/*
+ *
+ *
+ * --- components
+ * --- components
+ *
+ *
+ * */
+
+import { Search } from "./search/Search";
+
+export const Components = {
+    Search,
+};
