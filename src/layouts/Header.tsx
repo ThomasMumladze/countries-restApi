2ae7b133cf91c722ac071226b404ba00b356c2f8
Thomas Mumladze <a.mumladze@sda.gov.ge>
2024-09-26 16:52:26 +0400

    add header

---
+++ src/layouts/Header.tsx
@@ -0,0 +1,41 @@
+import "./style.scss";
+
+import * as Icon from "react-bootstrap-icons";
+
+import Ui from "../components";
+
+interface Props {
+    isDarkMode: boolean;
+    handleDarkMode: () => void;
+}
+
+export const Header = (props: Props) => {
+    const { isDarkMode, handleDarkMode } = props;
+
+    return (
+        <header
+            style={{
+                backgroundColor: isDarkMode ? "#2b3743" : "#FFF",
+                boxShadow: isDarkMode ? "0 0 18px 0 #222" : "0 0 18px 0 #ccc",
+            }}
+        >
+            <div>
+                <Ui.H1 style={{ color: isDarkMode ? "#fff" : "#222" }}>Where in the world?</Ui.H1>
+                <Ui.Button
+                    onClick={handleDarkMode}
+                    style={{ color: isDarkMode ? "rgb(250, 250, 250)" : "rgb(47, 71, 117)" }}
+                >
+                    {!isDarkMode ? (
+                        <>
+                            <Icon.Moon /> dark mode
+                        </>
+                    ) : (
+                        <>
+                            <Icon.Sun /> light mode
+                        </>
+                    )}
+                </Ui.Button>
+            </div>
+        </header>
+    );
+};
