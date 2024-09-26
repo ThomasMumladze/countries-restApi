da4ffd5f87f9270cc2d6be636cf1fc9107e8747e
Thomas Mumladze <a.mumladze@sda.gov.ge>
2024-09-26 17:13:38 +0400

    input ui

---
+++ src/components/Ui/Input.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Input = styled.input`
+    border: none;
+    background-color: transparent;
+    height: 100%;
+    outline: none;
+    width: 100%;
+`;
+
+export default Input;
