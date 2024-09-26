701dabdd103ef524491d32269023f43d4bf52f7a
Thomas Mumladze <a.mumladze@sda.gov.ge>
2024-09-26 16:53:12 +0400

    change body bg darkMode

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
 import "./App.css";
+import { useEffect } from "react";
+
+import { Home } from "./Pages/Home";
+
+import useDarkMode from "./hooks/useDarkMode";
 
 function App() {
-    return <></>;
+    const { isDarkMode, handleDarkMode } = useDarkMode();
+
+    useEffect(() => {
+        if (isDarkMode) {
+            document.body.style.backgroundColor = "#1c2730";
+        } else {
+            document.body.style.backgroundColor = "#ffffff";
+        }
+    }, [isDarkMode]);
+
+    return (
+        <>
+            <Home isDarkMode={isDarkMode} handleDarkMode={handleDarkMode} />
+        </>
+    );
 }
 
 export default App;
