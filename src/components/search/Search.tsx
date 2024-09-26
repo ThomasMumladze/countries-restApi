2aa295cf74699c76fccfa1d325d6c6baae1b7888
Thomas Mumladze <a.mumladze@sda.gov.ge>
2024-09-26 17:14:03 +0400

    search  add

---
+++ src/components/search/Search.tsx
@@ -0,0 +1,30 @@
+import "./style.scss";
+
+import * as Icon from "react-bootstrap-icons";
+
+import Ui from "..";
+
+interface Props {
+    isDarkMode: boolean;
+}
+
+export const Search = (props: Props) => {
+    const { isDarkMode } = props;
+
+    return (
+        <div
+            className="search"
+            style={{
+                backgroundColor: isDarkMode ? "#2b3743" : "#FFF",
+                border: isDarkMode ? "solid 1px transparent" : "solid 1px#151c22",
+            }}
+        >
+            <Icon.Search fill={isDarkMode ? "#fff" : "#000"} />
+            <Ui.Input
+                type="text"
+                placeholder="Search for countries"
+                style={{ color: isDarkMode ? "#fff" : "#000" }}
+            />
+        </div>
+    );
+};
