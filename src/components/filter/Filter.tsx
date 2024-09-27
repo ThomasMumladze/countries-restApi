1ce6ce15cad19caf355b08384d4002d263ec9da0
Thoma Mumladze <mumladzea4@gmail.com>
2024-09-27 22:52:26 +0400

    add background  hover effect

--- src/components/filter/Filter.tsx
+++ src/components/filter/Filter.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./style.scss";
 
 import * as Icon from "react-bootstrap-icons";
@@ -9,18 +10,33 @@ interface Props {
 export const Filter = (props: Props) => {
     const { isDarkMode } = props;
 
+    const [bgColor, setBgColor] = useState<string>("");
+
+    const handleMouseOver = () => {
+        if (isDarkMode) {
+            setBgColor("#2e3840");
+        } else {
+            setBgColor("#e3e5e9");
+        }
+    };
+
+    const handleMouseOut = () => {
+        setBgColor("");
+    };
+
     return (
-        <div className="filter">
+        <div className="filter" onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
             <div
                 className="container"
                 style={{
                     border: isDarkMode ? "solid 1px rgba(250, 250, 250, 0.5)" : "solid 1px rgba(47, 71, 117, 0.5)",
                     color: isDarkMode ? "#fff" : "#000",
+                    backgroundColor: bgColor,
                 }}
             >
                 <Icon.FilterLeft />
                 <p>
-                    fiilter by <span>: </span>
+                    filter by <span>: </span>
                 </p>
                 <Icon.CaretDown />
             </div>
