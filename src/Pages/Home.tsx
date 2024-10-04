fa674bcf1fc1a5e039fd850ce3c331d11314360f
Thoma Mumladze <mumladzea4@gmail.com>
2024-10-05 01:17:52 +0400

    add return btn

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,9 +1,11 @@
 import "./style.scss";
+
+import * as Icon from "react-bootstrap-icons";
 import { useEffect, useState } from "react";
 
 import { Header } from "../layouts/Header";
 
-import { Components } from "../components";
+import Ui, { Components } from "../components";
 
 import Api from "../api/Api";
 
@@ -15,6 +17,7 @@ interface Props {
 export const Home = (props: Props) => {
     const { isDarkMode, handleDarkMode } = props;
     const [allCountries, setAllCountries] = useState<null | []>(null);
+    const [countryCode, setCountryCode] = useState<string>("");
     const [searchCountry, setSearchCountry] = useState<string>("");
     const [searchRegion, setSearchRegion] = useState<string>("");
     const [error, setError] = useState<boolean | null>(null);
@@ -36,6 +39,14 @@ export const Home = (props: Props) => {
             });
     };
 
+    const viewCountryCode = (cca2: string) => {
+        setCountryCode(cca2);
+    };
+
+    const returnToCountries = () => {
+        setCountryCode("");
+    };
+
     useEffect(() => {
         let apiCall: any;
 
@@ -43,6 +54,8 @@ export const Home = (props: Props) => {
             apiCall = Api.getCountryByName(searchCountry);
         } else if (searchRegion) {
             apiCall = Api.getCountryByRegion(searchRegion);
+        } else if (countryCode) {
+            apiCall = Api.getCountryByCca2code(countryCode);
         } else {
             apiCall = Api.getAllCountries();
         }
@@ -60,27 +73,43 @@ export const Home = (props: Props) => {
         return () => {
             document.removeEventListener("keydown", handleKeyDown);
         };
-    }, [searchCountry, searchRegion]);
+    }, [searchCountry, searchRegion, countryCode]);
 
     return (
         <div className="home-page">
             <Header isDarkMode={isDarkMode} handleDarkMode={handleDarkMode} />
 
             <div style={{ maxWidth: "1200px", margin: "0 auto", padding: "0 52px" }}>
-                <div className="countries-search ">
-                    <Components.Search
-                        error={error}
-                        value={searchCountry}
-                        onChange={setSearchCountry}
-                        isDarkMode={isDarkMode}
-                    />
-                    <div className="countries-filter">
-                        <Components.Filter isDarkMode={isDarkMode} setSearchRegion={setSearchRegion} />
+                {allCountries && allCountries?.length > 1 ? (
+                    <div className="countries-search ">
+                        <Components.Search
+                            error={error}
+                            value={searchCountry}
+                            onChange={setSearchCountry}
+                            isDarkMode={isDarkMode}
+                        />
+                        <div className="countries-filter">
+                            <Components.Filter isDarkMode={isDarkMode} setSearchRegion={setSearchRegion} />
+                        </div>
                     </div>
-                </div>
+                ) : (
+                    <div className="returnBtn">
+                        <Ui.Button
+                            onClick={returnToCountries}
+                            style={{ color: isDarkMode ? "rgb(250, 250, 250)" : "rgb(47, 71, 117)" }}
+                        >
+                            <Icon.ArrowLeftShort /> back to countries
+                        </Ui.Button>
+                    </div>
+                )}
 
                 <div className="allCountries">
-                    <Components.CountryCard allCountries={allCountries} error={error} isDarkMode={isDarkMode} />
+                    <Components.CountryCard
+                        allCountries={allCountries}
+                        error={error}
+                        isDarkMode={isDarkMode}
+                        viewCountryCode={viewCountryCode}
+                    />
                 </div>
             </div>
         </div>
--- src/components/Ui/Button.ts
+++ src/components/Ui/Button.ts
@@ -3,6 +3,7 @@ import styled from "styled-components";
 const Button = styled.button`
     background-color: transparent;
     border: solid 1px;
+    cursor: pointer;
     font-family: "Nunito Sans", Helvetica, Arial, sans-serif;
     padding: 5px 15px;
     user-select: none;
