ed868974b68016dff1bbe35009de29bfefaf7953
Thoma Mumladze <mumladzea4@gmail.com>
2024-10-05 01:17:07 +0400

    reload add

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -20,7 +20,9 @@ export const Header = (props: Props) => {
             }}
         >
             <div>
-                <Ui.H1 style={{ color: isDarkMode ? "#fff" : "#222" }}>Where in the world?</Ui.H1>
+                <Ui.H1 style={{ color: isDarkMode ? "#fff" : "#222" }} onClick={() => window.location.reload()}>
+                    Where in the world?
+                </Ui.H1>
                 <Ui.Button
                     onClick={handleDarkMode}
                     style={{ color: isDarkMode ? "rgb(250, 250, 250)" : "rgb(47, 71, 117)" }}
